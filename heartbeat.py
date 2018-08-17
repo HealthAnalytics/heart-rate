e6efafa9ce5a08e365b77db3b3d0c2f5003fb1aa
Daniel Ardila <df.ardila328@gmail.com>
2018-08-17 23:43:45 +0200

    Loads video, calculates the mean per channel and plot it

--- heartbeat.py
+++ heartbeat.py
@@ -6,7 +6,25 @@ from matplotlib import pyplot as plt
 filename = './data/heartbeat_da.avi'
 vid = imageio.get_reader(filename)
 n_frames = vid.get_length()
-print(n_frames)
+mean_r = np.zeros(n_frames)
+mean_g = np.zeros(n_frames)
+mean_b = np.zeros(n_frames)
+time_indx = np.arange(n_frames)
+
+indx = 0
 for img in vid:
-    #imgplot = plt.imshow(img)
-    pylab.imshow(img)
+    np_img = np.array(img)
+    mean_r[indx] = np.mean(img[:, :, 0])
+    mean_g[indx] = np.mean(img[:, :, 1])
+    mean_b[indx] = np.mean(img[:, :, 2])
+    indx = indx + 1
+
+fig_red = plt.figure(),  plt.plot(time_indx, mean_r, '-r')
+fig_gre = plt.figure(),  plt.plot(time_indx, mean_g, '-g')
+fig_blu = plt.figure(),  plt.plot(time_indx, mean_b, '-b')
+plt.show()
+
+
+
+
+
